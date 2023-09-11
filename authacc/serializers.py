dc6d5a59a94134fb66a77696c2cec0d8d7a7299f
Sandeep Nadh <snxxt23@gmail.com>
2023-09-11 16:59:25 +0530

    user registration

---
+++ authacc/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from . models import *
+import re
+
+class UserRegistrationSerializers(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
+    class Meta:
+        model = User
+        fields = ['username','email','password','password2']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+    #validating password and confirm password while registration
+    def validate(self,data):
+        password = data.get('password')
+        password2 = data.get('password2')
+        username = data.get('username')
+        if password != password2:
+            raise serializers.ValidationError('Passwords Mismatching')
+        elif re.match(r"^\d{1,3}",username):
+            raise serializers.ValidationError('Username should starts with Alphabets')
+        return data
--- authacc/views.py
+++ authacc/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from . serializers import UserRegistrationSerializers
+from . models import *
 
-# Create your views here.
+class UserRegistrationView(APIView):
+    def post(self,request,format=None):
+        serializer = UserRegistrationSerializers(data=request.data)
+        if serializer.is_valid():
+            User.objects.create_user(
+                username = serializer.validated_data['username'],
+                email = serializer.validated_data['email'],
+                password = serializer.validated_data['password'],
+            )
+            return Response({"Message":"Registration Complete","User":serializer.data},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
